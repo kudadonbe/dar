9541fbc3701a53cc289cf1f8e421a8242c74c5ad
Hussain Shareef <hussain.shareef@gmail.com>
2022-11-21 05:27:41 +0500

    coding along the way

---
+++ models/data_models.js
@@ -0,0 +1,67 @@
+
+
+/**
+ * Product
+ * id
+ * name
+ * description
+ */
+
+/**
+ * product-entry
+ * product_id
+ * size_id
+ * colour_id
+ * material_id
+ * sku
+ * qty
+ */
+
+
+/**
+ * size
+ * id
+ * size_value
+ */
+
+
+/**
+ * colour
+ * id
+ * colour_value
+ */
+
+/**
+ * material
+ * id
+ * material_value
+ */
+
+
+
+/**
+ * phone_type
+ * type_id
+ * phone_type_name
+ */
+
+
+//product data for search results
+
+let sProduct = {
+    name: "A4 Paper",
+    details: "Double A papers",
+    stock: 5,
+    image: [
+        "img_url_0",
+        "img_url_1",
+        "img_url_2",
+    ],
+}
+
+let userInfo = {
+    name: "Hussain Shareef",
+    designation: "Assistant Council Officer",
+    role: "Stock keeper",
+    avator: "imgUrl"
+}
--- notes.js
+++ notes.js
@@ -5,3 +5,25 @@
 //     next()
 // }
 
+
+/** 
+ * 7 Habit of highly effective people
+ * 1 - Be proactive.                                        |
+ * 2 - Begin with the End in mind.                          | Becoiming independent self, Self Mastery
+ * 3 - Put first things first                               |
+ * 
+ * 4 - Think win-win                                        | 
+ * 5 - Seek first to understand and, then to be understood  | Interdependent
+ * 6 - Synergy                                              | 
+ * 
+ * 7 - Sharpen the saw
+ * 
+ * 
+ * */
+
+
+/**
+ * 1. Serach item localy
+ * 2. display available items
+ */
+
